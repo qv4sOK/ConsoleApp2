f21f4f6a23ea8ef3d843f0b67e7b17688759ee31
qv4sOK <mobail3c@gmail.com>
2022-04-20 11:15:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(short i = 0; i < 10; i++)
+            
+                if (i > 5)
+                    Console.WriteLine("El: " + i);
+        }
+    }
+}
